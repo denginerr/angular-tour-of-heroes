5319a9bb07fa09bc924ca7f6949c0d319a20b426
denginerr <denginerr@gmail.com>
2019-09-19 17:23:20 +0700

    First Commit Angular
    Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Monkey King' },
+    { id: 12, name: 'Pudge' },
+    { id: 13, name: 'Techies' },
+    { id: 14, name: 'Slardar' },
+    { id: 15, name: 'Lina' },
+    { id: 16, name: 'Riki' },
+    { id: 17, name: 'Invoker' },
+    { id: 18, name: 'Wraith King' },
+    { id: 19, name: 'Sven' }
+]
